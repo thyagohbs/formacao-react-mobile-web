1b7b5f54e4c526d310185847f4b2414b0c5793f1
Thyago Henrique <thyagobs.al@gmail.com>
2023-11-22 12:41:46 -0300

    update

--- voll-mobile/src/Login.tsx
+++ voll-mobile/src/Login.tsx
@@ -1,63 +1,39 @@
-import { VStack, Image, Text, Box, FormControl,Input, Button, Link } from 'native-base'
+import { VStack, Image, Text, Box, Link } from 'native-base'
 import { TouchableOpacity } from 'react-native';
 import Logo from './assets/Logo.png'
+import { Botao } from './componentes/Botao';
+import { EntradaTexto } from './componentes/EntradaTexto';
+import { Titulo } from './componentes/Titulo';
 
 export default function Login() {
   return (
     <VStack flex={1} alignItems="center" justifyContent="center" p={5}>
       <Image source={Logo} alt="Logo Voll" />
 
-      <Text
-        fontSize="2xl"
-        fontWeight="bold"
-        color="gray.500"
-        textAlign="center"
-        mt={5}
-      >
+      <Titulo>
         Faça login em sua conta
-      </Text>
+      </Titulo>
       <Box>
-        <FormControl mt={3}>
-          <FormControl.Label>Email</FormControl.Label>
-          <Input 
-            placeholder='Insira seu endereço de email'
-            size='lg'
-            w="100%"
-            borderRadius='lg'
-            bgColor='gray.100'
-            shadow={3}
-          />
-        </FormControl>
-        <FormControl mt={3}>
-          <FormControl.Label>Senha</FormControl.Label>
-          <Input 
-            placeholder='Insira sua senha'
-            size='lg'
-            w="100%"
-            borderRadius='lg'
-            bgColor='gray.100'
-            shadow={3}
-          />
-        </FormControl>
+        <EntradaTexto
+          label="Email"
+          placeholder="Insira seu endereço de e-mail"
+        />
+        <EntradaTexto
+          label="Senha"
+          placeholder="Insira sua senha"
+        />
       </Box>
-      <Button
-        w="100%"
-        bg="blue.800"
-        mt={10}
-        borderRadius="lg"
-      >
-        Entrar
-      </Button>
+      <Botao>Entrar</Botao>
 
-      <Link href='https://www.alura.com.br'>
+      <Link href='https://www.alura.com.br' mt={2}>
         Esqueceu sua senha?
       </Link>
 
-      <Box w="100%" flexDirection="row" justifyContent="center">
+      <Box w="100%" flexDirection="row" justifyContent="center" mt={8}>
         <Text>Ainda não tem cadastro? </Text>
         <TouchableOpacity>
           <Text color="blue.500">
-          Faça seu cadastro!
+            Faça seu cadastro!
           </Text>
         </TouchableOpacity>
       </Box>
